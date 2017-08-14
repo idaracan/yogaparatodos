e1b1abbf626978079bb7fdfd82d357840961cd71
Ivan Ramirez <ivandarioramirezcantillo@gmail.com>
2017-08-14 14:23:36 -0500

    Solved sidebar in admin page bug, added toggling function in rails implementation

---
+++ yptblog/app/assets/javascripts/admin/admin.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    // Moves a div downwards
+    function bringDown(){
+        document.getElementById("modifymenu").classList.add('movement');
+    }
+    // Closes the sidebar menu
+    $("#menu-close").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+    // Opens the sidebar menu
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+    // Visibility togglers
+    $("#services").click(function(e){
+      e.preventDefault();
+      $("#InfoGeneral").css("display","none");
+      //$("#Servicios").css("display","inline");
+    });
+    $("#infogen").click(function(e){
+      e.preventDefault();
+      $("#InfoGeneral").css("display","inline");
+      //$("#Servicios").css("display","none");
+    });
+});
--- yptblog/config/routes.rb
+++ yptblog/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'admin/index'
+  resources :admin
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'stylish_portfolios/index'
-  
+
   root 'stylish_portfolios#index'
 end
