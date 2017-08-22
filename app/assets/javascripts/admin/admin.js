6422b0e55d82adf301ece0b1b84b32738364b07f
Ivan Ramirez <ivandarioramirezcantillo@gmail.com>
2017-08-22 17:44:21 -0500

    added section Servicios on the admin page

--- app/assets/javascripts/admin/admin.js
+++ app/assets/javascripts/admin/admin.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
-    // Moves a div downwards
+    /* Moves a div downwards
     function bringDown(){
         document.getElementById("modifymenu").classList.add('movement');
-    }
+    }*/
+    $("#AddService").click(function(e) {
+        e.preventDefault();
+        $("#modifymenu").toggleClass("movement");
+    });
     // Closes the sidebar menu
     $("#menu-close").click(function(e) {
         e.preventDefault();
@@ -17,11 +21,11 @@ $(document).ready(function(){
     $("#services").click(function(e){
       e.preventDefault();
       $("#InfoGeneral").css("display","none");
-      //$("#Servicios").css("display","inline");
+      $("#Servicios").css("display","inline");
     });
     $("#infogen").click(function(e){
       e.preventDefault();
       $("#InfoGeneral").css("display","inline");
-      //$("#Servicios").css("display","none");
+      $("#Servicios").css("display","none");
     });
 });
